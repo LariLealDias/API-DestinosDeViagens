0adc4f792925d657a2225f21ba213787e4f032cc
Lari <larileal@hotmail.com.br>
2023-07-19 17:46:31 -0300

    feat: creating the DTO layer to Destination

---
+++ API-DestinosDeViagens/API-DestinosDeViagens/Data/Dtos/DtoDestination/ReadDestionationDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_DestinosDeViagens.Data.Dtos.DtoDestination;
+
+public class ReadDestionationDto
+{
+    public int Id { get; set; }
+    public string PhotoPath { get; set; }
+    public string Title { get; set; }
+    public double Price { get; set; }
+}
--- API-DestinosDeViagens/API-DestinosDeViagens/Data/Dtos/DtoDestination/UpdateDestinationDto.cs
+++ API-DestinosDeViagens/API-DestinosDeViagens/Data/Dtos/DtoDestination/UpdateDestinationDto.cs
@@ -1,15 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace API_DestinosDeViagens.Models;
+namespace API_DestinosDeViagens.Data.Dtos.DtoDestination;
 
-public class DestinationModel
+public class UpdateDestinationDto
 {
-    [Key]
-    public int Id { get; set; }
-
     [Required(ErrorMessage = "PhotoPath field can't be empty")]
     public string PhotoPath { get; set; }
-    
+
     [Required(ErrorMessage = "Title field can't be empty")]
     [MaxLength(50, ErrorMessage = "Title field can't ultrapass 50 characters")]
     public string Title { get; set; }
