ac5395f003398bac98df409609c3be48d695a7cb
Lari <larileal@hotmail.com.br>
2023-08-17 16:08:50 -0300

    feat: creating a new model

---
+++ API-DestinosDeViagens/API-DestinosDeViagens/Models/RoadTripModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Hosting;
+using System.ComponentModel.DataAnnotations;
+
+namespace API_DestinosDeViagens.Models;
+
+public class RoadTripModel
+{
+    [Key]
+    public int Id { get; set; }
+    public int TotalDays { get; set; }
+    public string Attractions { get; set; }
+    public string AverageCostOfAccommodation { get; set; }
+    public string AverageCostOfAlimentation { get; set; }
+}
