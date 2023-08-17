2d8f5f1b4831b73fa7429d74643d0ebd515aa415
Lari <larileal@hotmail.com.br>
2023-08-17 16:11:35 -0300

    feat: generating a Migration to new Model

---
+++ API-DestinosDeViagens/API-DestinosDeViagens/Migrations/20230817191042_AddingANewTable.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API_DestinosDeViagens.Migrations
+{
+    public partial class AddingANewTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
