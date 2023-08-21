b51fa6b1d2207fe93125ff46389265ba0ce057e6
Lari <larileal@hotmail.com.br>
2023-08-21 17:53:02 -0300

    feat: generating type one-to-one relationship

---
+++ API-DestinosDeViagens/API-DestinosDeViagens/Migrations/20230821205157_FixingTheRelationshipBetweenRoadTripModelAndDestinationModel.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API_DestinosDeViagens.Migrations
+{
+    public partial class FixingTheRelationshipBetweenRoadTripModelAndDestinationModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RoadTripModel",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    TotalDays = table.Column<int>(type: "int", nullable: false),
+                    Sights = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    AverageAccommodationPrice = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    AverageMealPrice = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    DestinationModelId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoadTripModel", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RoadTripModel_Destinations_DestinationModelId",
+                        column: x => x.DestinationModelId,
+                        principalTable: "Destinations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoadTripModel_DestinationModelId",
+                table: "RoadTripModel",
+                column: "DestinationModelId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RoadTripModel");
+        }
+    }
+}
